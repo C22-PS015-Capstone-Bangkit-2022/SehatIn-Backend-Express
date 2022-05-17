31bee8620b39185a3a6999d6ca5338d4fd3be465
I Gusti Agung VIvekananda <igustiagungvivekananda@gmail.com>
2022-05-17 17:31:59 +0700

    Revert "Add firebase-admin and middleware package"
    This reverts commit f22407570825d63753e3f70fd1275bef4d079266.

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -1,51 +1,51 @@
-const user = require("../controllers/user.controller");
-const disease = require("../controllers/disease.controller");
+const user = require('../controllers/user.controller');
+const disease = require('../controllers/disease.controller');
 const articles = require("../controllers/article.controller");
 const foods = require("../controllers/food.controller");
 const screening_question = require("../controllers/screening_question.controller");
 const images = require("../controllers/image.controller");
 const upload = require("../middleware/upload");
 
-module.exports = (app) => {
-  var router = require("express").Router();
+module.exports = app => {
+    var router = require("express").Router();
 
-  /* DISEASE */
-  router.get("/disease", disease.findAll);
+    /* DISEASE */
+    router.get("/disease", disease.findAll)
 
-  /* USER */
-  router.get("/user", user.findAll);
+    /* USER */
+    router.get("/user", user.findAll);
 
-  /* SCREEENING_QUESTION */
-  router.get("/screening-question", screening_question.findAll);
+    /* SCREEENING_QUESTION */
+    router.get("/screening-question", screening_question.findAll);
+  
+    /* ARTICLE*/
+    // Retrieve all Articles with id
+    router.get("/articles", articles.getAllArticles);
 
-  /* ARTICLE*/
-  // Retrieve all Articles with id
-  router.get("/articles", articles.getAllArticles);
+    // Retrieve a single Article with id
+    router.get("/articles/:id", articles.getArticleById);
 
-  // Retrieve a single Article with id
-  router.get("/articles/:id", articles.getArticleById);
+    // Create a new article
+    router.post("/articles", articles.addArticle);
 
-  // Create a new article
-  router.post("/articles/new", articles.addArticle);
+    // Update an Article
+    router.put("/articles/:id", articles.updateArticle)
 
-  // Update an Article
-  router.put("/articles/:id", articles.updateArticle);
+    // Delete an Article
+    router.delete("/articles/:id", articles.deleteArticle);
 
-  // Delete an Article
-  router.delete("/articles/delete/:id", articles.deleteArticle);
+    /*FOOD*/
+    // Create a food
+    router.post("/food", foods.addFood);
 
-  /*FOOD*/
-  // Create a food
-  router.post("/food", foods.addFood);
+    // Get All food
+    router.get("/food", foods.getAllFoods);
 
-  // Get All food
-  router.get("/food", foods.getAllFoods);
+    // delete a food
+    router.delete("/food/:id", foods.deleteFood);
 
-  // delete a food
-  router.delete("/food/:id", foods.deleteFood);
+    // upload images
+    router.post("/upload", images.multer.single("file"), images.uploadFile);
 
-  // upload images
-  router.post("/upload", images.multer.single("file"), images.uploadFile);
-
-  app.use("/v1/", router);
-};
+    app.use("/v1/", router);
+}
--- src/swagger.js
+++ src/swagger.js
@@ -4,11 +4,11 @@ const swaggerUi = require("swagger-ui-express");
 
 // Basic Meta Informations about our API
 const options = {
-  definition: {
-    openapi: "3.0.0",
-    info: { title: "Sehatin API", version: "1.0.0" },
-  },
-  apis: ["./src/routes/routes.js", "./src/database/Workout.js"],
+    definition: {
+        openapi: "3.0.0",
+        info: { title: "Sehatin API", version: "1.0.0" },
+    },
+    apis: ["./src/routes/routes.js", "./src/database/Workout.js"],
 };
 
 // Docs in JSON format
@@ -16,16 +16,16 @@ const swaggerSpec = swaggerJSDoc(options);
 
 // Function to setup our docs
 const swaggerDocs = (app, port) => {
-  // Route-Handler to visit our docs
-  app.use("/v1/docs", swaggerUi.serve, swaggerUi.setup(swaggerSpec));
-  // Make our docs in JSON format available
-  app.get("/v1/docs.json", (req, res) => {
-    res.setHeader("Content-Type", "application/json");
-    res.send(swaggerSpec);
-  });
-  console.log(
-    `Version 1 Docs are available on http://localhost:${port}/v1/docs`
-  );
+    // Route-Handler to visit our docs
+    app.use("/v1/docs", swaggerUi.serve, swaggerUi.setup(swaggerSpec));
+    // Make our docs in JSON format available
+    app.get("/v1/docs.json", (req, res) => {
+        res.setHeader("Content-Type", "application/json");
+        res.send(swaggerSpec);
+    });
+    console.log(
+        `Version 1 Docs are available on http://localhost:${port}/v1/docs`
+    );
 };
 
 module.exports = { swaggerDocs };
