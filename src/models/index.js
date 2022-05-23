4cef41579934de536f0efc03c05bb854b96a6510
I Gusti Agung VIvekananda <igustiagungvivekananda@gmail.com>
2022-05-23 14:10:39 +0700

    Fix foreignKey for food

--- src/models/index.js
+++ src/models/index.js
@@ -30,7 +30,7 @@ db.foodGoodFor = require("./foodGoodFor.model")(sequelize, Sequelize);
 db.foodBadFor = require("./foodBadFor.model")(sequelize, Sequelize);
 
 db.disease.hasMany(db.foodGoodFor, {foreignKey: 'id_disease'})
-db.foodGoodFor.hasOne(db.foods, {foreignKey: 'id_makanan'})
+db.foodGoodFor.hasOne(db.foods, {foreignKey: 'id_food'})
 db.disease.hasMany(db.foodBadFor, {as:"badFor"})
 // db.foodGoodFor.belongsTo(db.disease, {
 //     foreignKey: "id_penyakit",
