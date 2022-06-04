69703a2affcba51f52dc9e38877628d26d2f5cca
I Gusti Agung VIvekananda <igustiagungvivekananda@gmail.com>
2022-06-04 13:10:12 +0700

    Add midtrans initiation library

---
+++ src/config/midtrans.js
@@ -0,0 +1,20 @@
+const midtransClient = require("midtrans-client");
+let serverKey = "";
+let snap = new midtransClient.Snap();
+const forceProduction = false;
+if (process.env.NODE_ENV !== "production" && !forceProduction) {
+  //sandbox
+  serverKey = process.env.MIDTRANS_SB_SERVER_KEY;
+  snap.apiConfig.isProduction = false;
+  snap.apiConfig.serverKey = serverKey;
+  snap.apiConfig.clientKey = process.env.MIDTRANS_SB_CLIENT_KEY;
+} else {
+  //production
+  serverKey = process.env.MIDTRANS_PROD_SERVER_KEY;
+  snap.apiConfig.isProduction = true;
+  snap.apiConfig.serverKey = serverKey;
+  snap.apiConfig.clientKey = process.env.MIDTRANS_PROD_SERVER_KEY;
+}
+
+export const midtransServerKey = serverKey;
+export default snap;
